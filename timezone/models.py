d5eb10e784d6d544d3822f433ed19675cc9c253e
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-02-25 23:39:03 +0900

    Refactor to be compatible with drf

--- timezone/models.py
+++ timezone/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Timezone(models.Model):
     owner = models.OneToOneField('auth.User', related_name='timezone', primary_key=True, on_delete=models.CASCADE)
     timezone = models.CharField(max_length=32, default='UTC')
+
+    def __str__(self):
+        return self.timezone
